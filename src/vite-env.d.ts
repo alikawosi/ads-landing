757af9326cf0267262bc9e6fc8c293e3984bc5ad
AliKawosi <alikawosi.work@gmail.com>
2025-05-30 04:23:59 +0100

    refactor: Clean up supabase client and update environment variable definitions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,16 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_TERMLY_UUID: string;
   readonly VITE_TERMLY_PRIVACY_ID: string;
   readonly VITE_TERMLY_TERMS_ID: string;
+  readonly VITE_TERMLY_UUID: string;
+  readonly VITE_DEMO_URL: string;
+  readonly VITE_APP_URL: string;
+  readonly VITE_YEARLY_SUBSCRIPTION_PRICE_DISCOUNT: string;
+  readonly VITE_APP_PRO_PRICE: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+
   // add more env variables here
 }
 
